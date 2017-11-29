f08f805248b2264227b616911e02e657ea2e7aab
evagelosvar <evagelosvar@yahoo.com>
2017-11-29 15:31:26 +0200

    Update blog.js

--- docs/js/blog.js
+++ docs/js/blog.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
          alert("data");
-$("body").append("<div id='top'><h1>hello world</h1></div>");
+$("body").append("<div id=top ><h1>hello world</h1></div>");
 $("#top").load('test.html'); 
 });
