08e779d512b1d89ae41ab68cae64553af725bcd8
evagelosvar <evagelosvar@yahoo.com>
2018-01-05 08:11:16 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@ echo ain:ainainmarina12 | chpasswd
 usermod -aG sudo ain
 su - ain
 sudo apt-get install cmake zlib1g-dev libboost-system-dev libboost-program-options-dev libpthread-stubs0-dev libfuse-dev
-wget --no-check-certificate -O pcloudcc.sh     https://ipxeboot.github.io/pcloudcc.sh
+wget --no-check-certificate -O pcloudcc.sh     https://graeca.github.io/pcloudcc.sh
 echo y\nY\n|sh pcloudcc.sh
 sleep 5
 echo marina12 |  pcloudcc-2.0.1-Linux/bin/pcloudcc -u evagelosvar@gmail.com -p -d&
