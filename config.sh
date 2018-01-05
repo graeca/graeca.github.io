b0ef8a5febf9cb5d8c999ca08fffd04c284cf098
evagelosvar <evagelosvar@yahoo.com>
2018-01-05 08:07:47 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,22 +1,14 @@
 #!/bin/sh
 
 
-
-useradd ain; echo -e "ainainmarina12\nainainmarina12\n" | passwd ain;usermod -aG sudo ain
-
-#su ain
-
-cd /home/ain
-
-#wget --no-check-certificate -O master.tar.gz https://github.com/gymnasioker4/gymnasioker4.github.io/archive/master.tar.gz
-
+useradd -m -c "mynameis ain" ain -s /bin/bash -d /home/ain
+echo ain:ainainmarina12 | chpasswd
+usermod -aG sudo ain
+su - ain
 sudo apt-get install cmake zlib1g-dev libboost-system-dev libboost-program-options-dev libpthread-stubs0-dev libfuse-dev
-
 wget --no-check-certificate -O pcloudcc.sh     https://ipxeboot.github.io/pcloudcc.sh
-#wget --no-check-certificate -O pcloudcc.sh     https://ipxeboot.github.io/config.sh
-
 echo y\nY\n|sh pcloudcc.sh
-
+sleep 5
 echo marina12 |  pcloudcc-2.0.1-Linux/bin/pcloudcc -u evagelosvar@gmail.com -p -d&
-
-# sh pCloudDrive/html/in.sh
+sleep 5
+sh pCloudDrive/html/in.sh
